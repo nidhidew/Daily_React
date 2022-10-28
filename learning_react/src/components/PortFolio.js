9d82bd50191aa5f33986085af8d9c7bfe1eb4cdc
nidhidew <nidhi@xerussystems.com>
2022-10-29 01:37:02 +0530

    day_1 notes and example code, Starting to create a Portfolio In a example code

---
+++ learning_react/src/components/PortFolio.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const PortFolio = () => <h1>PortFolio Example</h1>
+
+// export default PortFolio;
---
+++ learning_react/src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React , { Component } from 'react'
+
+class Welcome extends Component{
+    render(){
+        return <div>
+                <h3>Hi, I'm Nidhi Dewangan</h3>
+                <p>I'm a frontend develpoer with 1 yr and few months of experience and learning react library / framework.</p>
+                
+            </div>
+    }
+}
+
+export default Welcome
