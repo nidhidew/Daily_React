31953c53589304b07b4ec940c5a6b18c7e07b30f
Nidhi Dewangan <41151235+nidhidew@users.noreply.github.com>
2023-06-22 01:49:54 +0530

    Delete learning_react directory

--- learning_react/src/components/Greetings.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Greetings(props) {
-    console.log(props);
-    return <h1>Hello {props.name}</h1>
-}
-
-
-export default Greetings;
--- learning_react/src/components/PortFolio.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const PortFolio = () => <h1>PortFolio Example</h1>
-
-// export default PortFolio;
